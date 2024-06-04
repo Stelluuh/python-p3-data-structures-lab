27b522cf6da68e6e988c0a861de324f5882b5084
stella <sftamesis13@gmail.com>
2024-06-03 17:31:02 -0700

    complete challenge 1 and 2 out of 7

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,10 +17,11 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    #iterate through each dict and grab the value. Ex: "Green Curry, Thai, 9"
+    return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
     pass
